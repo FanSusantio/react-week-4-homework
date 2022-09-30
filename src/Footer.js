46b599e71006e9065cef51d3e869550ad0dbfd86
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-09-30 13:20:28 +1000

    Added footer with links to Github code

---
+++ src/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by
+      <a
+        href="https://www.linkedin.com/in/fan-susantio-62227919/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Fan Susantio
+      </a>
+      and is
+      <a
+        href="https://github.com/FanSusantio/react-week-4-homework"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        open-sourced on GitHub
+      </a>
+      and
+      <a
+        href="https://delicate-churros-88f420.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        hosted on Netlify
+      </a>
+      .<br />
+    </footer>
+  );
+}
