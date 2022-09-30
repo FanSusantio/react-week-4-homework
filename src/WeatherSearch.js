9e3a468e5606e2a5c389baefc71bf8a990509c8f
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-09-30 13:00:55 +1000

    Create WeatherSearch.js
    Copied in weather app script

---
+++ src/WeatherSearch.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch(props) {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "300c5b69978f9b056c3bb931ff9c185d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  if (weather.temperature) {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="search" placeholder={city} onChange={updateCity} />
+          <input type="submit" value="Search" />
+        </form>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
+}
